6d0116ee490d1698cd27629e194c270916a7e522
Ture <ture.cl.business@gmail.com>
2021-06-05 13:55:09 +0900

    Remove fragile test

--- src/tests/test_players.py
+++ src/tests/test_players.py
@@ -25,7 +25,6 @@ def test_heuristic():
             'expected_adjacency_white': 54,
             'expected_distance_black': 46,
             'expected_distance_white': 46,
-            'expected_heuristic': 10000.0,
         },
         {
             'value': InitialPosition.GERMAN_DAISY.value,
@@ -34,7 +33,24 @@ def test_heuristic():
             'expected_adjacency_white': 48,
             'expected_distance_black': 42,
             'expected_distance_white': 42,
-            'expected_heuristic': 10000.0,
+        },
+        {
+            'value': [
+                [Marble.BLANK] * 5,
+                [Marble.BLANK] * 6,
+                [Marble.BLANK] * 7,
+                [Marble.BLANK] * 3 + [Marble.BLACK] * 2 + [Marble.BLANK] * 3,
+                [Marble.BLANK] * 3 + [Marble.BLACK] * 3 + [Marble.BLANK] * 3,
+                [Marble.BLANK] * 3 + [Marble.BLACK] * 2 + [Marble.BLANK] * 3,
+                [Marble.BLANK] * 7,
+                [Marble.BLANK] * 6,
+                [Marble.BLANK] * 5,
+            ],
+            'in_turn': Player.BLACK,
+            'expected_adjacency_black': 24,
+            'expected_adjacency_white': 0,
+            'expected_distance_black': 6,
+            'expected_distance_white': 0,
         },
     ]
 
@@ -45,8 +61,8 @@ def test_heuristic():
         game.turn = board['in_turn']
         algorithm = players.AlphaBetaSimple(game, game.turn.value)
         counts = algorithm._count_heuristics(game)
+        print(counts)
         assert counts['sum_adjacency'][Player.BLACK.value] == board['expected_adjacency_black']
         assert counts['sum_adjacency'][Player.WHITE.value] == board['expected_adjacency_white']
         assert counts['sum_distance'][Player.BLACK.value] == board['expected_distance_black']
         assert counts['sum_distance'][Player.WHITE.value] == board['expected_distance_white']
-        assert algorithm._heuristic(game) == board['expected_heuristic']
